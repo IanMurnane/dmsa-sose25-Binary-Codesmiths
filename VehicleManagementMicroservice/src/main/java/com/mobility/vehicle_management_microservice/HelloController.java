179826368d4b4fdff54c87112031f8615301e999
Ian Murnane <ian.murnane@fh-dortmund.de>
2025-05-20 11:50:05 +0200

    hello world

---
+++ VehicleManagementMicroservice/src/main/java/com/mobility/vehicle_management_microservice/HelloController.java
@@ -0,0 +1,13 @@
+package com.mobility.vehicle_management_microservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
---
+++ VehicleManagementMicroservice/src/test/java/com/mobility/vehicle_management_microservice/VehicleManagementMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobility.vehicle_management_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleManagementMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
