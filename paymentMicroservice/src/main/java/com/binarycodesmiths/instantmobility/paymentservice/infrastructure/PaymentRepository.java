43a53c448dfa1a703df6e08abbc6cbfc69a80cb7
Sraboni Bhuiyan <35885263+sraboni-bhuiyan@users.noreply.github.com>
2025-05-22 20:13:12 +0200

    Delete paymentMicroservice directory

--- paymentMicroservice/src/main/java/com/binarycodesmiths/instantmobility/paymentservice/infrastructure/PaymentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.binarycodesmiths.instantmobility.paymentservice.infrastructure;
-
-import com.binarycodesmiths.instantmobility.paymentservice.domain.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, String> {
-    Payment findByBookingId(Long bookingId);
-}
