b2c1254202f6ad67c6c71d602105aefd8380ec3a
sraboni-bhuiyan <bhuiyansraboni@gmail.com>
2025-05-30 13:33:38 +0200

    updated all services with eureka connection

--- VehicleManagementMicroservice/src/main/java/com/mobility/vehicle_management_microservice/VehicleManagementServiceApplication.java
+++ VehicleManagementMicroservice/src/main/java/com/mobility/vehicle_management_microservice/VehicleManagementServiceApplication.java
@@ -2,9 +2,11 @@ package com.mobility.vehicle_management_microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableWebMvc
 public class VehicleManagementServiceApplication {
     public static void main(String[] args) {
