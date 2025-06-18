3af6e496441ae381d4c0e799012794bb4c1576a8
Ian Murnane <ian.murnane@fh-dortmund.de>
2025-06-18 12:56:53 +0200

    frontend - displaying map and vehicles from vehicle microservice

--- Frontend/src/api/apis.js
+++ Frontend/src/api/apis.js
@@ -1,19 +1,19 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: "http://localhost:8080/api", // replace with your backend URL
-});
+const UserAPI = axios.create({baseURL: "http://localhost:8080/api"});
+const VehicleAPI = axios.create({baseURL: "http://localhost:8082"});
 
-export const getUsers = () => API.get("/users");
-
+export const getUsers = () => UserAPI.get("/users");
 export const loginUser = (email, password) => {
   const params = new URLSearchParams();
   params.append("email", email);
   params.append("password", password);
 
-  return API.post("/auth/login", params, {
+  return UserAPI.post("/auth/login", params, {
     headers: {
       "Content-Type": "application/x-www-form-urlencoded"
     }
   });
 };
+
+export const getVehicles = () => VehicleAPI.get("/vehicles");
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { getVehicles } from "../api/apis";
+import L from "leaflet";
+
+// Optional: Custom marker icon fix for Leaflet default icons
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+    iconUrl: require("leaflet/dist/images/marker-icon.png"),
+    shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+});
 
 const Home = () => {
-  return <h1>Home Page</h1>;
+    const [vehicles, setVehicles] = useState([]);
+
+    useEffect(() => {
+        getVehicles()
+            .then((res) => {
+                console.log(res.data);
+                const parsed = res.data.map((v) => {
+                    const [lat, lng] = v.location.split(" ").map(Number);
+                    console.log({lat, lng});
+                    return { lat, lng };
+                });
+                setVehicles(parsed);
+            })
+            .catch((err) => {
+                console.error("API Error:", err);
+            });
+    }, []);
+
+    return (
+        <div style={{ height: "100vh", width: "100%" }}>
+            <MapContainer center={[51.491444, 7.414111]} zoom={13} style={{ height: "100%", width: "100%" }}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {vehicles.map((v, idx) => (
+                    <Marker key={idx} position={[v.lat, v.lng]}>
+                        <Popup>Vehicle at ({v.lat}, {v.lng})</Popup>
+                    </Marker>
+                ))}
+            </MapContainer>
+        </div>
+    );
 };
 
 export default Home;
