6de8a92758c037a4ab80d09e9f86fe0e2650dec6
sraboni-bhuiyan <bhuiyansraboni@gmail.com>
2025-06-24 10:37:45 +0200

    a small fix

--- Frontend-Provider/src/App.js
+++ Frontend-Provider/src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from './pages/Dashboard';
 import Vehicles from './pages/Vehicles';
@@ -9,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/*" element={<Dashboard />} />
         <Route path="/vehicles" element={<Vehicles />} />
         <Route path="/bookings" element={<Bookings />} />
         <Route path="/feedback" element={<Feedback />} />
